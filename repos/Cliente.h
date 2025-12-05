a8c1f2ebe4dbfa45ba11cd2d542cee953d33ca7c
Alejandro Tenorio <143746192+cody090309003@users.noreply.github.com>
2025-12-05 02:11:15 -0600

    Delete repos directory

--- repos/Cliente.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Usuario.h"
-
-class Cliente : public Usuario {
-public:
-    Cliente(int id, const std::string& nombre, const std::string& email, const std::string& password)
-        : Usuario(id, nombre, email, password) {
-    }
-};
