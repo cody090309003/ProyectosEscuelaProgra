063f1a51da553cdf9d74cf9c458580214a6820d8
Alejandro Tenorio <143746192+cody090309003@users.noreply.github.com>
2025-12-06 00:58:48 -0600

    Delete VoletosAvionCliente directory

--- VoletosAvionCliente/repos/Admin.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Usuario.h"
-
-class Admin : public Usuario {
-public:
-    Admin(int id, const std::string& nombre, const std::string& email, const std::string& password)
-        : Usuario(id, nombre, email, password) {
-    }
-};
